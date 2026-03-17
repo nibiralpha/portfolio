6a0825c0f42ed91d23f3d7abf1436be57603365e
Nibir Alam <nibiralpha@gmail.com>
2026-03-18 05:53:39 +0600

    about us section animation added

--- src/app/Component/AboutUsComponent/AboutUsComponent.js
+++ src/app/Component/AboutUsComponent/AboutUsComponent.js
@@ -1,19 +1,65 @@
+"use client";
+
+import { useEffect, useRef } from "react";
 import "./AboutUs.css";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function AboutUsComponent() {
+  const sectionRef = useRef(null);
+  const leftRef = useRef(null);
+  const rightRef = useRef(null);
+
+  useEffect(() => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.set(leftRef.current, { y: 0 });
+  gsap.set(rightRef.current, { y: 0 });
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: sectionRef.current,
+      start: "top 100",
+      end: "+=2000",
+      scrub: 1.2,
+      pin: true,
+      anticipatePin: 1,
+    },
+  });
+
+  tl.to(leftRef.current, {
+    y: -100,
+    ease: "none",
+  });
+
+  tl.to(
+    rightRef.current,
+    {
+      y: 100,
+      ease: "none",
+    },
+    0
+  );
+}, []);
+
   return (
     <div className="about_section">
       <div className="scroll_container">
-        <img className="mouse" src={"/images/mouse.svg"}></img>
+        <img className="mouse" src={"/images/mouse.svg"} />
         <div className="scroll_text">SCROLL</div>
       </div>
-      <div className="about_us">
-        <div className="title">ABOUT IS</div>
+
+      <div className="about_us" ref={sectionRef}>
+        <div className="title">ABOUT US</div>
 
         <div className="about_detail_container">
           <div className="about_images">
-            <img className="left_image" src={"/images/left.webp"}></img>
-            <img className="right_image" src={"/images/right.svg"}></img>
+            <img ref={leftRef} className="left_image" src="/images/left.webp" />
+            <img
+              ref={rightRef}
+              className="right_image"
+              src="/images/right.svg"
+            />
           </div>
 
           <div className="about_detail">
