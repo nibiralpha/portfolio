e1c989079d8ee12ea5780a3b25e112f4e9a9cb08
Nibir Alam <nibiralpha@gmail.com>
2026-03-20 05:20:28 +0600

    list animation done

--- src/app/Component/ListComponent/ListComponent.js
+++ src/app/Component/ListComponent/ListComponent.js
@@ -1,8 +1,39 @@
+"use client";
+
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef } from "react";
+
 import "./listComponent.css";
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function ListComponent({ number, title, subTitle }) {
+  const container = useRef();
+
+  useGSAP(
+    () => {
+      gsap.from(".left_contents", {
+        x: 400,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.2, 
+        scrollTrigger: {
+          scrub: 1,
+          trigger: ".list",
+          start: "top 95%", 
+          end: "top 50%",
+          toggleActions: "play none none reverse",
+        },
+      });
+    },
+    { scope: container },
+  );
+
   return (
-    <div>
+    <div ref={container}>
       <div className="list">
         <div className="left_contents">
           <div className="index">{number}</div>
