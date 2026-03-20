44be5b679e0a3827700de3163e3b440dd0fb3b54
Nibir Alam <nibiralpha@gmail.com>
2026-03-21 04:56:07 +0600

    projects section menu change animation done

--- src/app/Component/ProjectsComponent/ProjectComponent.js
+++ src/app/Component/ProjectsComponent/ProjectComponent.js
@@ -13,21 +13,24 @@ export default function OurApproachComponent() {
   useGSAP(
     () => {
       const images = gsap.utils.toArray(".project img");
+      const menuItems = gsap.utils.toArray(".menu_text");
 
+      gsap.set(images, { position: "relative" });
       gsap.set(images.slice(1), {
         yPercent: 100,
         position: "absolute",
         top: 0,
         left: 0,
       });
-     
-      gsap.set(images[0], { position: "relative" });
+
+      gsap.set(menuItems, { color: "rgb(115, 115, 115)" });
+      gsap.set(menuItems[0], { color: "#ffffff" });
 
       const tl = gsap.timeline({
         scrollTrigger: {
           trigger: mainRef.current,
-          start: "top 5%", 
-          end: "+=300%", 
+          start: "top 5%",
+          end: "+=300%",
           scrub: 1,
           pin: true,
           anticipatePin: 1,
@@ -43,15 +46,18 @@ export default function OurApproachComponent() {
             yPercent: 0,
             ease: "none",
           },
-          "+=0.2",
-        );
+          "+=0.5",
+        )
+
+          .to(menuItems[i], { color: "#ffffff", duration: 0.2 }, "<")
+          .to(menuItems[i - 1], { color: "#414141", duration: 0.2 }, "<");
       });
     },
     { scope: mainRef },
   );
 
   return (
-    <div className="projects_container">
+    <div style={{ overflowX: "hidden", width: "100%" }}>
       <div className="projects" ref={mainRef}>
         <div className="project_section">
           <div className="project_menu">
@@ -62,13 +68,14 @@ export default function OurApproachComponent() {
               <div className="menu_text">Studio Kline</div>
             </div>
           </div>
-          <div className="project_gallery">
+
+          <div className="project_gallery" style={{ overflow: "hidden" }}>
             <div className="project_slider">
               <div className="project">
-                <img className="project_img" src="/images/project1.webp" alt="p1" />
-                <img className="project_img" src="/images/project2.png" alt="p2" />
-                <img className="project_img" src="/images/project3.webp" alt="p3" />
-                <img className="project_img" src="/images/project4.webp" alt="p4" />
+                <img src="/images/project1.webp" alt="p1" />
+                <img src="/images/project2.png" alt="p2" />
+                <img src="/images/project3.webp" alt="p3" />
+                <img src="/images/project4.webp" alt="p4" />
               </div>
             </div>
           </div>
